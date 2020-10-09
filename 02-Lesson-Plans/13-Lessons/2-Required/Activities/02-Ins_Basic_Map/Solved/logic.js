acc5f333c5fe964c211d9871a9bbc57eea91873a
vyu821 <67933865+vyu821@users.noreply.github.com>
2020-10-08 19:26:12 -0700

    adding week 13 activities

---
+++ 02-Lesson-Plans/13-Lessons/2-Required/Activities/02-Ins_Basic_Map/Solved/logic.js
@@ -0,0 +1,16 @@
+// Creating our initial map object
+// We set the longitude, latitude, and the starting zoom level
+// This gets inserted into the div with an id of 'map'
+var myMap = L.map("map", {
+  center: [45.52, -122.67],
+  zoom: 13
+});
+
+// Adding a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+  id: 'mapbox/streets-v11',
+	accessToken: API_KEY
+}).addTo(myMap);
---
+++ 02-Lesson-Plans/13-Lessons/2-Required/Activities/07-Ins_Layers/Solved/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiZGdvbGRlbiIsImEiOiJjazVvNzcwc3QxaThkM2pwbHN6M2Z0MmRuIn0.eaiMyfIaUo97omT9YKaSIg";
